f3bbe5e8402781c6faea3406ac56a4429eba9e40
usman khan <usmankhan@usmans-MBP.home>
2019-06-14 10:40:56 -0400

    usman basic list

---
+++ usman-list.py
@@ -0,0 +1,11 @@
+input_string = input("Enter all users name  ")
+
+family_list  = input_string.split(",")
+print("Printing all user names")
+for name in family_list:
+    print(name)
+input_string = input("Enter all users age  ")
+family_list  = input_string.split(",")
+print("Printing all user age")
+for age in family_list:
+    print(age)
